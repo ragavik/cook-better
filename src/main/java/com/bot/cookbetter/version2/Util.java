86bc49c8354948e98cff639f9bdd8e0bab7e759d
ksivasu <ksivasu@ncsu.edu>
2018-04-06 12:47:21 -0400

    ExtractIngredients Commit

--- src/main/java/com/bot/cookbetter/version2/Util.java
+++ src/main/java/com/bot/cookbetter/version2/Util.java
@@ -11,6 +11,19 @@ public class Util {
 
     //@kashyap
     public static String extractIngredients(String naturalQuery){
+            String lower = naturalQuery.toLowerCase();
+            String array1[]= lower.split(" ");
+            for (String temp: array1){
+                System.out.println(temp);
+            }
+            for (String temp: array1 )
+               // Find a match from table of ingredients
+                // select * from ingredients where value = temp;
+                //if a match is found store it in a string to return.
+            }
+            // return csv : apple,milk,orange
+            return null;
+        }
 
         // return csv : apple,milk,orange
         return null;
